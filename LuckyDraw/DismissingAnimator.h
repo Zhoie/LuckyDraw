97604b4b37e4f87ad5d1b0d7838bd3ef7a993fa2
Ray <sovw100@icloud.com>
2015-04-06 17:28:24 +0800

    抽奖添加了动画，弹出另一个view显示抽奖信息。

---
+++ LuckyDraw/DismissingAnimator.h
@@ -0,0 +1,14 @@
+//
+//  DismissingAnimator.h
+//  LuckyDraw
+//
+//  Created by ZHOUTAO on 15/4/6.
+//  Copyright (c) 2015年 iTeam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface DismissingAnimator : NSObject <UIViewControllerAnimatedTransitioning>
+
+@end
